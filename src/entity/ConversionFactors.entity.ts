c64c3f984a7acf2f808bdd16738b37f504570eb3
Anders Gravesen <ang@obton.com>
2024-11-18 14:53:13 +0100

    Unique constraint added on ConversionFactors

--- src/entity/ConversionFactors.entity.ts
+++ src/entity/ConversionFactors.entity.ts
@@ -1,10 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, Unique } from "typeorm";
 import { Category } from "./Categories.entity";
 import { Cutout } from "./Cutouts.entity";
 import { Produce } from "./Produce.entity";
 import { CommonEntity } from "./Common.entity";
 
 @Entity({ name: "ConversionFactors" })
+@Unique(["Category", "Produce", "Cutout"])
 export class ConversionFactor extends CommonEntity{
   @ManyToOne(() => Category, category => category.conversionFactors, { cascade: true, onDelete: "CASCADE", onUpdate: "CASCADE" })
   Category: Category;
